ded915666d1318578036262dd955ae4381f86385
Jeremy Holstein <jjjrmy@gmail.com>
2024-12-23 00:01:30 -0500

    refactor InteractsWithPivot

--- src/Relations/Concerns/InteractsWithPivot.php
+++ src/Relations/Concerns/InteractsWithPivot.php
@@ -7,157 +7,42 @@ use Alvin0\RedisModel\Builder;
 trait InteractsWithPivot
 {
     /**
-     * The foreign key of the parent model.
-     *
-     * @var string
-     */
-    protected $foreignKey;
-
-    /**
-     * The associated key of the relation.
-     *
-     * @var string
-     */
-    protected $relatedKey;
-
-    /**
-     * Attach a model to the parent.
-     *
-     * @param  mixed  $id
-     * @param  array  $attributes
-     * @param  bool  $touch
-     * @return void
-     */
-    public function attach($id, array $attributes = [], $touch = true)
-    {
-        if ($this->using) {
-            $this->attachUsingCustomClass($id, $attributes);
-        } else {
-            // Always use SQL for pivot operations
-            $this->newPivotStatement()->insert(
-                $this->formatAttachRecords($this->parseIds($id), $attributes)
-            );
-        }
-
-        if ($touch) {
-            $this->touchIfTouching();
-        }
-    }
-
-    /**
      * Create a new pivot statement for inserting records.
      *
      * @return \Illuminate\Database\Query\Builder
      */
     public function newPivotStatement()
     {
+        // Always use SQL query builder for pivot operations
         return $this->query->getQuery()->newQuery()->from($this->table);
     }
 
     /**
-     * Format the sync / attach records.
+     * Create a new query builder for the pivot table.
      *
-     * @param  array  $ids
-     * @param  array  $attributes
-     * @return array
+     * @return \Illuminate\Database\Query\Builder
      */
-    protected function formatAttachRecords($ids, array $attributes)
+    public function newPivotQuery()
     {
-        $records = [];
+        $query = $this->newPivotStatement();
 
-        $hasTimestamps = ($this->hasPivotColumn($this->createdAt()) ||
-                         $this->hasPivotColumn($this->updatedAt()));
-
-        foreach ($ids as $id) {
-            $records[] = $this->formatAttachRecord($id, $id, $attributes, $hasTimestamps);
+        foreach ($this->pivotWheres as $arguments) {
+            $query->where(...$arguments);
         }
 
-        return $records;
-    }
-
-    /**
-     * Format a single attach record.
-     *
-     * @param  mixed  $key
-     * @param  mixed  $value
-     * @param  array  $attributes
-     * @param  bool   $hasTimestamps
-     * @return array
-     */
-    protected function formatAttachRecord($key, $value, $attributes, $hasTimestamps)
-    {
-        list($id, $attributes) = $this->extractAttachIdAndAttributes($key, $value, $attributes);
-
-        return array_merge(
-            $this->baseAttachRecord($id, $hasTimestamps),
-            $attributes
-        );
-    }
-
-    /**
-     * Get the base attach record.
-     *
-     * @param  mixed  $id
-     * @param  bool   $hasTimestamps
-     * @return array
-     */
-    protected function baseAttachRecord($id, $hasTimestamps)
-    {
-        $record = [
-            $this->getRelatedPivotKeyName() => $id,
-            $this->getForeignPivotKeyName() => $this->parent->getKey(),
-        ];
-
-        if ($hasTimestamps) {
-            $record[$this->createdAt()] = $this->parent->freshTimestamp();
-            $record[$this->updatedAt()] = $this->parent->freshTimestamp();
+        foreach ($this->pivotWhereIns as $arguments) {
+            $query->whereIn(...$arguments);
         }
 
-        return $record;
+        foreach ($this->pivotWhereNulls as $arguments) {
+            $query->whereNull(...$arguments);
+        }
+
+        return $query->where($this->getQualifiedForeignPivotKeyName(), $this->parent->{$this->parentKey});
     }
 
     /**
-     * Get the foreign key for the relation.
-     *
-     * @return string
-     */
-    public function getForeignKeyName()
-    {
-        return $this->foreignKey;
-    }
-
-    /**
-     * Get the related key for the relation.
-     *
-     * @return string
-     */
-    public function getRelatedKeyName()
-    {
-        return $this->relatedKey;
-    }
-
-    /**
-     * Get the foreign pivot key name.
-     *
-     * @return string
-     */
-    public function getForeignPivotKeyName()
-    {
-        return $this->foreignPivotKey;
-    }
-
-    /**
-     * Get the related pivot key name.
-     *
-     * @return string
-     */
-    public function getRelatedPivotKeyName()
-    {
-        return $this->relatedPivotKey;
-    }
-
-    /**
-     * Parse the IDs into an array.
+     * Get all of the IDs from the given mixed value.
      *
      * @param  mixed  $value
      * @return array
@@ -177,18 +62,5 @@ trait InteractsWithPivot
         }
 
         return (array) $value;
-    }
-
-    /**
-     * Extract the id and extra attributes from the attach record.
-     *
-     * @param  mixed  $key
-     * @param  mixed  $value
-     * @param  array  $attributes
-     * @return array
-     */
-    protected function extractAttachIdAndAttributes($key, $value, array $attributes)
-    {
-        return [$value, $attributes];
     }
 }
